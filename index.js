e5fcc92d787ab8ec93d4bc820cc26a6125ebe188
Nico Rehwaldt <git_nikku@nixis.de>
2018-02-18 22:25:29 +0100

    chore(project): initial implementation

---
+++ index.js
@@ -0,0 +1,316 @@
+'use strict';
+
+/**
+ * Module Dependencies
+ */
+
+var iterator = require('dom-iterator');
+var selection = window.getSelection();
+
+
+/**
+ * Add selection / insert cursor.
+ *
+ * @param {Range} range
+ */
+function applyRange(range) {
+  selection.removeAllRanges();
+  selection.addRange(range);
+}
+
+module.exports.applyRange = applyRange;
+
+
+/**
+ * Get current document selection.
+ *
+ * @return {Selection}
+ */
+function getWindowSelection() {
+  return selection;
+}
+
+module.exports.getWindowSelection = getWindowSelection;
+
+
+/**
+ * Set cursor or selection position.
+ *
+ * @param {Element} el
+ * @param {SelectionRange} selection
+ */
+function setRange(el, selection) {
+
+  var range = createRange(el, selection);
+
+  applyRange(range);
+}
+
+module.exports.setRange = setRange;
+
+
+/**
+ * Get cursor or selection position.
+ *
+ * @param {Element} el
+ */
+function getRange(el) {
+
+  if (!selection.rangeCount) {
+    return null;
+  }
+
+  var focusNode = selection.focusNode;
+
+  if (el !== focusNode && !el.contains(focusNode)) {
+    return null;
+  }
+
+  var range = selection.getRangeAt(0);
+
+  var startContainer = range.startContainer;
+  var endContainer = range.endContainer;
+  var startOffset = range.startOffset;
+  var endOffset = range.endOffset;
+
+  var i = iterator(el.firstChild, el);
+
+  var next = i.node;
+  var isClosing = false;
+
+  var selectionStart = 0;
+  var count = 0;
+
+  function isBeforeEnd(node, referenceNode) {
+
+    if (arguments.length === 1) {
+      referenceNode = node;
+    }
+
+    return (
+      node.parentNode === endContainer &&
+      referenceNode == endContainer.childNodes[endOffset]
+    );
+
+  }
+
+  function isBeforeStart(node, referenceNode) {
+
+    if (arguments.length === 1) {
+      referenceNode = node;
+    }
+
+    return (
+      node.parentNode === startContainer &&
+      referenceNode == startContainer.childNodes[startOffset]
+    );
+
+  }
+
+  while (next) {
+
+    // start before node
+    if (isBeforeStart(next)) {
+      selectionStart = count;
+    }
+
+    // end before node
+    if (isBeforeEnd(next)) {
+      break;
+    }
+
+    if (!isClosing) {
+
+      // <br />, </div>, </p> node
+      if (isBr(next) || isDiv(next)) {
+        count++;
+      }
+    }
+
+    if (isClosing) {
+      if (isParagraph(next)) {
+        count++;
+      }
+    }
+
+    if (isText(next)) {
+
+      // #text node
+      if (startContainer === next) {
+        selectionStart = count + startOffset;
+      }
+
+      if (endContainer === next) {
+        count += endOffset;
+        break;
+      }
+
+      count += next.textContent.length;
+    }
+
+    if (isText(next) || isClosing) {
+
+      // start before node
+      if (isBeforeStart(next, next.nextSibling)) {
+        selectionStart = count;
+      }
+
+      // end before node
+      if (isBeforeEnd(next, next.nextSibling)) {
+        break;
+      }
+
+    }
+
+    next = i.next();
+    isClosing = i.closingTag;
+  }
+
+  // selection until end of text
+  return {
+    start: selectionStart,
+    end: count
+  };
+}
+
+module.exports.getRange = getRange;
+
+
+/**
+ * Annotate the given text with markers based on the
+ * given range.
+ *
+ * @param {String} text
+ * @param {SelectionRange} range
+ *
+ * @return {String} annotated text
+ */
+function annotateRange(text, range) {
+  var str;
+
+  if (range.start === range.end) {
+    str = (
+      text.substring(0, range.start) +
+      '|' +
+      text.substring(range.start)
+    );
+  } else {
+    str = (
+      text.substring(0, range.start) +
+      '<' +
+      text.substring(range.start, range.end) +
+      '>' +
+      text.substring(range.end)
+    );
+  }
+
+  return str;
+}
+
+module.exports.annotateRange = annotateRange;
+
+
+//////// helpers ///////////////////////////
+
+function createRange(el, selection) {
+
+  var start = selection.start;
+  var end = selection.end;
+
+  var range = document.createRange();
+
+  var i = iterator(el.firstChild, el);
+
+  var next = i.node;
+  var isClosing = false;
+
+  var count = 0;
+  var length;
+
+  while (next) {
+
+    if (count === start) {
+      if (isClosing) {
+        range.setStartAfter(next);
+      } else {
+        range.setStartBefore(next);
+      }
+    }
+
+    if (count === end) {
+      if (isClosing) {
+        range.setEndAfter(next);
+      } else {
+        range.setEndBefore(next);
+      }
+
+      return range;
+    }
+
+    if (!isClosing) {
+      if (isDiv(next) || isBr(next)) {
+        count++;
+      }
+    }
+
+    if (isClosing) {
+      if (isParagraph(next)) {
+        count++;
+      }
+    }
+
+    if (isText(next)) {
+
+      length = next.textContent.length;
+
+      if (count <= start && count + length > start) {
+        range.setStart(next, start - count);
+      }
+
+      if (count + length > end) {
+        range.setEnd(next, end - count);
+
+        return range;
+      }
+
+      count += length;
+    }
+
+    next = i.next();
+    isClosing = i.closingTag;
+  }
+
+  // out of range
+  if (count <= start) {
+    range.setStartAfter(el.lastChild);
+  }
+
+  range.setEndAfter(el.lastChild);
+
+  return range;
+}
+
+function isText(node) {
+  return node.nodeType === 3;
+}
+
+function isBr(node) {
+  return (
+    node.nodeType === 1 &&
+    node.nodeName === 'BR'
+  );
+}
+
+function isDiv(node) {
+  return (
+    node.nodeType === 1 &&
+    node.nodeName === 'DIV'
+  );
+}
+
+function isParagraph(node) {
+  return (
+    node.nodeType === 1 &&
+    node.nodeName === 'P'
+  );
+}
---
+++ test.js
@@ -0,0 +1,515 @@
+'use strict';
+
+var getRange = require('./').getRange,
+    setRange = require('./').setRange,
+    applyRange = require('./').applyRange,
+    getWindowSelection = require('./').getWindowSelection;
+
+
+describe('selection-rangee', function() {
+
+  var node;
+
+  beforeEach(function() {
+    node = document.createElement('div');
+
+    node.setAttribute('contenteditable', 'true');
+
+    document.body.appendChild(node);
+  });
+
+
+  afterEach(function() {
+    document.body.removeChild(node);
+
+    window.getSelection().removeAllRanges();
+  });
+
+
+  describe('getWindowSelection', function() {
+
+    it('should expose window selection', function() {
+      expect(getWindowSelection()).to.equal(window.getSelection());
+    });
+
+  });
+
+
+  describe('applyRange', function() {
+
+    it('should set selection range', function() {
+
+      // given
+      node.innerHTML = 'FOO<br>bar<br>';
+
+      var range = document.createRange();
+      range.setStart(node.childNodes[0], 1);
+      range.setEndAfter(node.childNodes[1]);
+
+      // when
+      applyRange(range);
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 1,
+        end: 4
+      });
+    });
+
+  });
+
+
+  describe('getRange', function() {
+
+    it('full range', function() {
+
+      // given
+      node.innerHTML = 'FOOaaaa';
+
+      var range = document.createRange();
+      range.setStartBefore(node.childNodes[0]);
+      range.setEndAfter(node.childNodes[0]);
+
+      applyRange(range);
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 0,
+        end: 7
+      });
+    });
+
+
+    it('in text', function() {
+
+      // given
+      node.innerHTML = 'FOOaaaa';
+
+      var range = document.createRange();
+      range.setStart(node.childNodes[0], 4);
+      range.setEnd(node.childNodes[0], 6);
+
+      applyRange(range);
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 4,
+        end: 6
+      });
+    });
+
+
+    it('cross line-break', function() {
+
+      // given
+      node.innerHTML = 'FOO<br>aaaa<br>';
+
+      var range = document.createRange();
+      range.setStart(node.childNodes[0], 2);
+      range.setEnd(node.childNodes[2], 3);
+
+      applyRange(range);
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 2,
+        end: 7
+      });
+    });
+
+
+    it('cross container', function() {
+
+      // given
+      node.innerHTML = 'FOO<div><br>aaaa</div><div class="ref"><br>bb</div>';
+
+      var range = document.createRange();
+      range.setStart(node.childNodes[0], 2);
+      range.setEndAfter(node.querySelector('.ref br'));
+
+      applyRange(range);
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 2,
+        end: 11
+      });
+    });
+
+
+    it('cross paragraphs', function() {
+
+      // given
+      node.innerHTML = '<p>FOO</p><p class="ref">aaaa</p>';
+
+      var range = document.createRange();
+      range.setStart(node.childNodes[0].childNodes[0], 2);
+      range.setEnd(node.querySelector('.ref').childNodes[0], 3);
+
+      applyRange(range);
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 2,
+        end: 7
+      });
+    });
+
+
+    describe('caret', function() {
+
+      it('in text / at start', function() {
+
+        // given
+        node.innerHTML = 'FOOaaaa';
+
+        var range = document.createRange();
+        range.setStartBefore(node.childNodes[0]);
+        range.setEndBefore(node.childNodes[0]);
+
+        applyRange(range);
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 0,
+          end: 0
+        });
+      });
+
+
+      it('in text / at end', function() {
+
+        // given
+        node.innerHTML = 'FOOaaaa';
+
+        var range = document.createRange();
+        range.setStartAfter(node.childNodes[0]);
+        range.setEndAfter(node.childNodes[0]);
+
+        applyRange(range);
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 7,
+          end: 7
+        });
+      });
+
+
+      it('in text / mid', function() {
+
+        // given
+        node.innerHTML = 'FOOaaaa';
+
+        var range = document.createRange();
+        range.setStart(node.childNodes[0], 5);
+        range.setEnd(node.childNodes[0], 5);
+
+        applyRange(range);
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 5,
+          end: 5
+        });
+      });
+
+
+      it('with line-break', function() {
+
+        // given
+        node.innerHTML = 'FOO<br>aaaa<br>';
+
+        var range = document.createRange();
+        range.setStart(node.childNodes[2], 1);
+        range.setEnd(node.childNodes[2], 1);
+
+        applyRange(range);
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 5,
+          end: 5
+        });
+      });
+
+
+      it('with containers', function() {
+
+        // given
+        node.innerHTML = 'FOO<div><br>aaaa</div><div><br>bb</div>';
+
+        var textNode = node.querySelector('div', node).childNodes[1];
+
+        var range = document.createRange();
+        range.setStartAfter(textNode);
+        range.setEndAfter(textNode);
+
+        applyRange(range);
+
+        // when
+        expect(getRange(node)).to.eql({
+          start: 9,
+          end: 9
+        });
+      });
+
+    });
+
+  });
+
+
+  describe('setRange', function() {
+
+    it('full', function() {
+
+      // given
+      node.innerHTML = 'FOOaaaa';
+
+      // when
+      setRange(node, {
+        start: 0,
+        end: 7
+      });
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 0,
+        end: 7
+      });
+    });
+
+
+    it('out of bounds', function() {
+
+      // given
+      node.innerHTML = 'FOOaaaa';
+
+      // when
+      setRange(node, {
+        start: 0,
+        end: 9
+      });
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 0,
+        end: 7
+      });
+    });
+
+
+    it('in text', function() {
+
+      // given
+      node.innerHTML = 'FOOaaaa';
+
+      // when
+      setRange(node, {
+        start: 4,
+        end: 6
+      });
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 4,
+        end: 6
+      });
+    });
+
+
+    it('cross line-break', function() {
+
+      // given
+      node.innerHTML = 'FOO<br>aaaa<br>';
+
+      // when
+      setRange(node, {
+        start: 2,
+        end: 7
+      });
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 2,
+        end: 7
+      });
+    });
+
+
+    it('cross container', function() {
+
+      // given
+      node.innerHTML = 'FOO<div><br>aaaa</div><div><br>bb</div>';
+
+      // when
+      setRange(node, {
+        start: 2,
+        end: 11
+      });
+
+      // when
+      expect(getRange(node)).to.eql({
+        start: 2,
+        end: 11
+      });
+    });
+
+
+    it('cross paragraphs', function() {
+
+      // given
+      node.innerHTML = '<p>FOO</p><p class="ref">aaaa</p>';
+
+      setRange(node, {
+        start: 2,
+        end: 7
+      });
+
+      // then
+      expect(getRange(node)).to.eql({
+        start: 2,
+        end: 7
+      });
+    });
+
+
+    it('cross container / out of bounds', function() {
+
+      // given
+      node.innerHTML = 'FOO<div><br>aaaa</div><div><br>bb</div>';
+
+      // when
+      setRange(node, {
+        start: 2,
+        end: 15
+      });
+
+      // when
+      expect(getRange(node)).to.eql({
+        start: 2,
+        end: 13
+      });
+    });
+
+
+    describe('caret', function() {
+
+      it('in text / at start', function() {
+
+        // given
+        node.innerHTML = 'FOOaaaa';
+
+        // when
+        setRange(node, {
+          start: 0,
+          end: 0
+        });
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 0,
+          end: 0
+        });
+      });
+
+
+      it('in text / at end', function() {
+
+        // given
+        node.innerHTML = 'FOOaaaa';
+
+        // when
+        setRange(node, {
+          start: 7,
+          end: 7
+        });
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 7,
+          end: 7
+        });
+      });
+
+
+      it('in text / at end / out of bounds', function() {
+
+        // given
+        node.innerHTML = 'FOOaaaa';
+
+        // when
+        setRange(node, {
+          start: 9,
+          end: 9
+        });
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 7,
+          end: 7
+        });
+      });
+
+
+      it('in text / mid', function() {
+
+        // given
+        node.innerHTML = 'FOOaaaa';
+
+        // when
+        setRange(node, {
+          start: 5,
+          end: 5
+        });
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 5,
+          end: 5
+        });
+      });
+
+
+      it('with line-break', function() {
+
+        // given
+        node.innerHTML = 'FOO<br>aaaa<br>';
+
+        // when
+        setRange(node, {
+          start: 5,
+          end: 5
+        });
+
+        // then
+        expect(getRange(node)).to.eql({
+          start: 5,
+          end: 5
+        });
+      });
+
+
+      it('with containers', function() {
+
+        // given
+        node.innerHTML = 'FOO<div><br>aaaa</div><div><br>bb</div>';
+
+        // when
+        setRange(node, {
+          start: 8,
+          end: 8
+        });
+
+        // when
+        expect(getRange(node)).to.eql({
+          start: 8,
+          end: 8
+        });
+      });
+
+    });
+
+  });
+
+});
